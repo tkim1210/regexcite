17497cb5c3e0175c5132f68f8d14d9f33fa94cba
tkim1210 <tkim12102004@gmail.com>
2026-04-03 16:52:56 -0700

    updated function with different params and tests

--- R/str_split_one.R
+++ R/str_split_one.R
@@ -1,9 +1,9 @@
 #' Split a string
 #'
-#' @param x A character vector with one element.
-#' @param split What to split on.
+#' @param string A character vector with, at most, one element.
+#' @inheritParams stringr::str_split
 #'
-#' @returns A character vector.
+#' @return A character vector.
 #' @export
 #'
 #' @examples
